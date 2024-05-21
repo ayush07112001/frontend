e161fd266c4a5da88bb64cc6d8652efbb7cb5ecc
ayush07112001 <ayushpatel0711.ap@gmail.com>
2024-05-21 17:20:59 +0530

    class

---
+++ day49_js/day40_js/index.js
@@ -0,0 +1,8 @@
+const h1=document.getElementsByTagName('h1')
+console.log("this is coming from js",h1)
+//h1[1].innerText="Ram"
+//h1[1].innerHTML=<p>Ram</p>
+
+
+const query=document.querySelector('div')
+    console.log("this is coming from query selector",query)
---
+++ day49_js/index.js
@@ -0,0 +1,18 @@
+const element=document.querySelector('section')
+console.log(element.children.length)
+for(let i=0;i<element.children.length;i++){
+    element.children[i].style.backgroundColor='grey'
+    element.children[i].style.margin='10px'
+    element.children[i].style.padding='10px'
+    
+    
+}
+// console.log(section.firstelementchild.innerText)
+
+// console.log(element.lastElementChild.innerText)
+
+// console.log(element.previousElementSibling)
+
+// console.log(element.nextElementSibling)
+
+document.body.replaceChild(h1,div)
