fd847bcf94ed2d7c9d5b64c6bc1325d3a8174fbf
ayush07112001 <ayushpatel0711.ap@gmail.com>
2024-05-28 17:25:43 +0530

    api

---
+++ day54_js/index.js
@@ -0,0 +1,8 @@
+// fetch Data from API 
+const fetchDataFromAPI=async()=>{
+    const api=await fetch("https://jsonplaceholder.typicode.com/photos");
+    const data=await api.json();
+    console.log(data)
+};
+fetchDataFromAPI();
+console.log("hello console")
